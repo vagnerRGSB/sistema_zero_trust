e1861ef21bfbf9935ea9f59a4f0a29630c6cc7b2
VagnerRGSB <vagnerrgsb@gmail.com>
2025-11-06 22:12:04 -0300

    criação migration, model e entity de usuário. Falta implementar toda lógica de localização do usuário

---
+++ app/Database/Migrations/2025-11-07-003231_UsuariosMigration.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UsuariosMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id"=>[
+                "type"=>"int",
+                "unsignet"=>true,
+                "auto_increment"=>true
+            ],
+            "nivel_acesso_id"=>[
+                "type"=>"int",
+                "unsigned"=>true
+            ],
+            "nome"=>[
+                "type"=>"varchar",
+                "constraint"=>50,
+            ],
+            "sobre_nome"=>[
+                "type"=>"varchar",
+                "constraint"=>50
+            ],
+            "bloqueado_ate"=>[
+                "type"=>"datetime",
+                "null"=>true,
+                "default"=>null
+            ],
+            "criado_em"=>[
+                "type"=>"datetime",
+                "null"=>true,
+                "default"=>null
+            ],
+            "atualizado_em"=>[
+                "type"=>"datetime",
+                "null"=>true,
+                "default"=>null
+            ],
+            "desabilitado_em"=>[
+                "type"=>"datetime",
+                "null"=>true,
+                "default"=>null
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id");
+        $this->forge->createTable("usuarios",true,["engine"=>"InnoDB"]);
+        $this->forge->addForeignKey("nivel_acesso_id","niveis_acessos","id","cascade","restrict","fk_nivel_acesso_has_usuario");
+    }
+
+    public function down()
+    {
+        $this->forge->dropForeignKey("usuarios","fk_nivel_acesso_has_usuario");
+        $this->forge->dropTable("usuarios",true);
+    }
+}
---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsuarioModel extends Model
+{
+    protected $table            = 'usuarios';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'App\Entities\UsuarioEntity';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        "nome",
+        "sobre_nome"
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'criado_em';
+    protected $updatedField  = 'atualizado_em';
+    protected $deletedField  = 'desabilitado_em';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
