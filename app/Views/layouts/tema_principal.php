84785d41dcc990d34f948be79738342a1294cecc
vagnerrgsb <vagnerrgsb@gmail.com>
2025-11-10 16:24:32 -0300

    atualização cache view

--- app/Views/layouts/tema_principal.php
+++ app/Views/layouts/tema_principal.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Sistema zero trust</title>
-
+    <?= view("layouts/partials/styles",[],["cache"=>60,"cache_name"=>"partials_styles"])?>
     <?= $this->renderSection("style") ?>
     
 </head>
 <body>
     <?= $this->renderSection("conteudo") ?>
 
+    <?= view("layouts/partials/scripts",[],["cache"=>60,"cache_name"=>"partials_scripts"]) ?>
     <?= $this->renderSection("script") ?>
+    
 </body>
 </html>
