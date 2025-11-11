626381a7556690fa5138b6a00db914c3e28ad6dd
vagnerrgsb <vagnerrgsb@gmail.com>
2025-11-11 09:32:06 -0300

    sigini atualizado

--- app/Views/layouts/partials/styles.php
+++ app/Views/layouts/partials/styles.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="<?= base_url('public/extras/css/bootstrap.min.css')?>">
-<link rel="stylesheet" href="<?= base_url('public/extras/css/style.css') ?>">
+<link rel="stylesheet" href="<?= base_url('extras/css/bootstrap.min.css')?>">
+<link rel="stylesheet" href="<?= base_url('extras/css/style.css') ?>">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-<link rel="stylesheet" href="<?= base_url('public/extras/libs/tempusdominus/css/tempusdominus-bootstrap-4.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('libs/tempusdominus/css/tempusdominus-bootstrap-4.min.css') ?>">
