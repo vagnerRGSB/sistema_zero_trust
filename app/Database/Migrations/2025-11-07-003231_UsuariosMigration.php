9d96e4679d5350bee02a97bc3aae33adc0900eb7
vagnerrgsb <vagnerrgsb@gmail.com>
2025-11-14 11:04:07 -0300

    atualização migration de usuario

--- app/Database/Migrations/2025-11-07-003231_UsuariosMigration.php
+++ app/Database/Migrations/2025-11-07-003231_UsuariosMigration.php
@@ -31,6 +31,22 @@ class UsuariosMigration extends Migration
                 "null"=>true,
                 "default"=>null
             ],
+            "endereco_cep_id"=>[
+                "type"=>"int",
+                "unsigned"=>true
+            ],
+            "numero"=>[
+                "type"=>"int",
+                "usigned"=>true
+            ],
+            "tipo_imovel_id"=>[
+                "type"=>"int",
+                "unsigned"=>true
+            ],
+            "complemento"=>[
+                "type"=>"varchar",
+                "constraint"=>60
+            ],
             "criado_em"=>[
                 "type"=>"datetime",
                 "null"=>true,
@@ -50,11 +66,16 @@ class UsuariosMigration extends Migration
         $this->forge->addPrimaryKey("id");
         $this->forge->createTable("usuarios",true,["engine"=>"InnoDB"]);
         $this->forge->addForeignKey("nivel_acesso_id","niveis_acessos","id","cascade","restrict","fk_nivel_acesso_has_usuario");
+        $this->forge->addForeignKey("endereco_cep_id","enderecos_ceps","id","cascade","restrict","fk_endereco_cep_has_usuario");
+        $this->forge->addForeignKey("tipo_imovel_id","tipos_imoveis","id","cascade","restrict","fk_tipo_imovel_has_usuario");
+
     }
 
     public function down()
     {
         $this->forge->dropForeignKey("usuarios","fk_nivel_acesso_has_usuario");
+        $this->forge->dropForeignKey("usuarios","fk_endereco_cep_has_usuario");
+        $this->forge->dropForeignKey("usuarios","fk_tipo_imovel_has_usuario");
         $this->forge->dropTable("usuarios",true);
     }
 }
