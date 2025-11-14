bbc9b4d0cd41161fc045ce256080a086645af36a
vagnerrgsb <vagnerrgsb@gmail.com>
2025-11-14 14:53:18 -0300

    implementando teste interface EnderecoCepService

--- app/Services/EnderecoCepServices.php
+++ app/Services/EnderecoCepServices.php
@@ -2,12 +2,31 @@
 
 namespace App\Services;
 
-class EnderecoService{
+use App\Interface\CrudInterface;
+
+class EnderecoService implements CrudInterface{
 
 private $endereco_cep;
 
 public function __construct(){
         
     }
+
+public function find(int $id){
+
+}
+public function findAll(){
+
+}
+public function create(array $data){
+
+}
+public function update(int $id, array $data){
+
+}
+public function delete(int $id){
+    
+}
+
 
 }
